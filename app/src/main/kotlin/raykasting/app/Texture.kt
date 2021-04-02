57bfd08a6d8d0660a19b89a07243037301ca90cd
Daniel André da Silva <dap1995@gmail.com>
2021-04-02 15:24:41 -0300

    Create Texture

---
+++ app/src/main/kotlin/raykasting/app/Texture.kt
@@ -0,0 +1,26 @@
+package raykasting.app
+
+import java.awt.image.BufferedImage
+import java.io.InputStream
+import javax.imageio.ImageIO
+
+class Texture(private val inputStream: InputStream, size: Int) {
+    private val pixels = IntArray(size * size)
+    init { load() }
+
+    private fun load() {
+        ImageIO.read(inputStream).getRGB(
+            startX = 0,
+            startY = 0,
+            rgbArray = pixels,
+            offset = 0
+        )
+    }
+}
+
+private fun BufferedImage.getRGB(
+    startX: Int,
+    startY: Int,
+    rgbArray: IntArray,
+    offset: Int
+): IntArray = this.getRGB(startX, startY, width, height, rgbArray, offset, width)
